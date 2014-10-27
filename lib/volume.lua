e84075c7df9945da021644352621fc30a3fd6271
Serghei Iakovlev <sadhooklay@gmail.com>
2014-10-27 08:53:33 +0200

    Used awful lib for commands

--- lib/volume.lua
+++ lib/volume.lua
@@ -13,25 +13,17 @@
 local popen    = io.popen
 local execute  = os.execute
 local tonumber = tonumber
+local awful = require("awful")
+
 local volume   = {}
 
-local function slurpcommand(cmd)
-  local pipe, error = popen(cmd, 'r')
-  if not pipe then
-    return pipe, error
-  end
-  local contents = pipe:read '*a'
-  pipe:close()
-  return contents
-end
-
 function volume.get()
-  local volume = slurpcommand 'ponymix get-volume'
+  local volume = awful.util.pread('ponymix get-volume')
   return tonumber(volume)
 end
 
 function volume.ismute()
-  return execute('ponymix is-muted') == 0
+  return awful.util.pread('ponymix is-muted') == 0
 end
 
 function volume.toggle()
@@ -41,12 +33,12 @@ function volume.toggle()
 end
 
 function volume.increment()
-  local volume = slurpcommand 'ponymix increase 5'
+  local volume = awful.util.pread('ponymix increase 5')
   return tonumber(volume)
 end
 
 function volume.decrement()
-  local volume = slurpcommand 'ponymix decrease 5'
+  local volume = awful.util.pread('ponymix decrease 5')
   return tonumber(volume)
 end
 
